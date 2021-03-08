188fec6e759151b0f2fcd6ef1c74a8ff7f96601a
Anders Genderskov Binder <heyanders@gmail.com>
2021-03-08 17:05:40 +0100

    Added A view and an Index

---
+++ Indexes/EquipmentTypeIndex.sql
@@ -0,0 +1,9 @@
+﻿Drop Index If Exists IX_Equipment_Type On dbo.Equipment
+
+
+Create NONCLUSTERED Index IX_Equipment_Type
+On dbo.Equipment (Type);
+
+
+--Select Type, COUNT(*) as Total From Equipment
+--Group by Type
---
+++ Views/CharacterSkills.sql
@@ -0,0 +1,12 @@
+﻿--Drop View If Exists dbo.CharacterSkills
+
+
+--Select p.Email From Profile p
+
+--Select cs.Character_Id, cs.Skill_Name From Characters_Skills cs
+
+Create View dbo.CharacterSkills
+AS
+	(Select c.Character_Id, c.Character_Name, cs.Skill_Name From Characters c
+	Inner join Characters_Skills cs
+	On cs.Character_Id = c.Character_Id)
