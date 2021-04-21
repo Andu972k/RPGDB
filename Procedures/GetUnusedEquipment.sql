99ec6e49626637e00fbffbe1d0e899ed945c030e
Anders Genderskov Binder <heyanders@gmail.com>
2021-04-21 17:38:24 +0200

    Refactored the stored procedure GetUnusedEquipment to reflect the change of moving equipmentType to its own table

--- Procedures/GetUnusedEquipment.sql
+++ Procedures/GetUnusedEquipment.sql
@@ -5,34 +5,34 @@ Create Procedure dbo.GetUnusedEquipment
 AS
 	Set NOCOUNT ON;
 	Begin
-		Select e.Equipment_Id, e.Name, e.Type From Equipment e
+		Select e.Equipment_Id, e.Name, e.EquipmentType From Equipment e
 		Left join Characters c
 		On e.Equipment_Id = c.Head
-		Where c.Head is NULL And Type = 'Head';
-		Select e.Equipment_Id, e.Name, e.Type From Equipment e
+		Where c.Head is NULL And EquipmentType = 'Head';
+		Select e.Equipment_Id, e.Name, e.EquipmentType From Equipment e
 		Left join Characters c
 		On e.Equipment_Id = c.Chest
-		Where c.Chest is NULL And Type = 'Chest';
-		Select e.Equipment_Id, e.Name, e.Type From Equipment e
+		Where c.Chest is NULL And EquipmentType = 'Chest';
+		Select e.Equipment_Id, e.Name, e.EquipmentType From Equipment e
 		Left join Characters c
 		On e.Equipment_Id = c.Hands
-		Where c.Hands is NULL And Type = 'Hands';
-		Select e.Equipment_Id, e.Name, e.Type From Equipment e
+		Where c.Hands is NULL And EquipmentType = 'Hands';
+		Select e.Equipment_Id, e.Name, e.EquipmentType From Equipment e
 		Left join Characters c
 		On e.Equipment_Id = c.Legs
-		Where c.Legs is NULL And Type = 'Legs';
-		Select e.Equipment_Id, e.Name, e.Type From Equipment e
+		Where c.Legs is NULL And EquipmentType = 'Legs';
+		Select e.Equipment_Id, e.Name, e.EquipmentType From Equipment e
 		Left join Characters c
 		On e.Equipment_Id = c.Feet
-		Where c.Feet is NULL And Type = 'Feet';
-		Select e.Equipment_Id, e.Name, e.Type From Equipment e
+		Where c.Feet is NULL And EquipmentType = 'Feet';
+		Select e.Equipment_Id, e.Name, e.EquipmentType From Equipment e
 		Left join Characters c
 		On e.Equipment_Id = c.Left_Hand
-		Where c.Left_Hand is NULL And Type = 'Left_Hand';
-		Select e.Equipment_Id, e.Name, e.Type From Equipment e
+		Where c.Left_Hand is NULL And EquipmentType = 'Left_Hand';
+		Select e.Equipment_Id, e.Name, e.EquipmentType From Equipment e
 		Left join Characters c
 		On e.Equipment_Id = c.Right_Hand
-		Where c.Right_Hand is NULL And Type = 'Right_Hand';
+		Where c.Right_Hand is NULL And EquipmentType = 'Right_Hand';
 	END;
 
 	--https://docs.microsoft.com/en-us/sql/t-sql/statements/create-procedure-transact-sql?view=sql-server-ver15
